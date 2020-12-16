de0e4ccaf216dfb5ef7729f705e8b48dfe85bfca
Roberto Arciniegas <rarci003@fiu.edu>
2020-12-15 22:01:14 -0500

    User registration functional

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,12 +1,82 @@
 <?php namespace App\Controllers;
 
+use App\Models\UserModel;
+
 class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		$data = [];
+		helper(['form']);
+
+		if ($this->request->getMethod() == 'post') {
+			$rules = [
+				'email' => 'required|min_length[6]|max_length[50]|valid_email',
+				'password' => 'required|min_length[8]|max_length[255]|validateUser[email,password]',
+			];
+
+			$errors = [
+				'password' => [
+					'validateUser' => 'Email or Password don\'t match'
+				]
+			];
+
+			if (! $this->validate($rules, $errors)) {
+				$data['validation'] = $this->validator;
+			}else{
+				$model = new UserModel();
+
+				$user = $model->where('email', $this->request->getVar('email'))
+											->first();
+
+				$this->setUserSession($user);
+				return redirect()->to('dashboard');
+
+			}
+		}
+
+		echo view('templates/header');
+		echo view( 'login');
+		echo view('templates/footer');
 	}
 
+	public function register(){
+		$data = [];
+		helper(['form']);
+
+		if ($this->request->getMethod() == 'post') {
+			//let's do the validation here
+			$rules = [
+				'firstname' => 'required|min_length[3]|max_length[20]',
+				'lastname' => 'required|min_length[3]|max_length[20]',
+				'email' => 'required|min_length[6]|max_length[50]|valid_email|is_unique[users.email]',
+				'password' => 'required|min_length[8]|max_length[255]',
+				'password_confirm' => 'matches[password]',
+			];
+
+			if (! $this->validate($rules)) {
+				$data['validation'] = $this->validator;
+			}else{
+				$model = new UserModel();
+
+				$newData = [
+					'firstname' => $this->request->getVar('firstname'),
+					'lastname' => $this->request->getVar('lastname'),
+					'email' => $this->request->getVar('email'),
+					'password' => $this->request->getVar('password'),
+				];
+				$model->save($newData);
+				$session = session();
+				$session->setFlashdata('success', 'Successful Registration');
+				return redirect()->to('/');
+
+			}
+		}
+
+		echo view('templates/header', $data);
+		echo view('register');
+		echo view('templates/footer');
+	}
 	//--------------------------------------------------------------------
 
 }
